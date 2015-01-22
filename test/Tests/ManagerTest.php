1cacec985d622fb8663dafdbfc384766f4009b8b
Stephen Young <young.steveo@gmail.com>
2015-01-21 23:09:15 -0500

    Added tests and implemented `Manager::invoke`

--- test/Tests/ManagerTest.php
+++ test/Tests/ManagerTest.php
@@ -22,4 +22,139 @@ class ManagerTest extends \PHPUnit_Framework_TestCase {
         $bucket = $this->getMock('Zumba\Swivel\Bucket', null, [$map]);
         $this->assertInstanceOf('Zumba\Swivel\Manager', $manager->setBucket($bucket));
     }
+
+    public function testInvokeOneParamEnabled() {
+        $config = $this->getMock('Zumba\Swivel\Config');
+        $manager = $this->getMock('Zumba\Swivel\Manager', ['forFeature'], [$config]);
+        $builder = $this
+            ->getMockBuilder('Zumba\Swivel\Builder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $manager
+            ->expects($this->once())
+            ->method('forFeature')
+            ->with('Test')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('addBehavior')
+            ->with('version.a', 'abc')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('defaultBehavior')
+            ->with(null)
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue('abc'));
+
+        $this->assertEquals('abc', $manager->invoke('Test.version.a', 'abc'));
+    }
+
+    public function testInvokeOneParamDisabled() {
+        $config = $this->getMock('Zumba\Swivel\Config');
+        $manager = $this->getMock('Zumba\Swivel\Manager', ['forFeature'], [$config]);
+        $builder = $this
+            ->getMockBuilder('Zumba\Swivel\Builder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $manager
+            ->expects($this->once())
+            ->method('forFeature')
+            ->with('Test')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('addBehavior')
+            ->with('version.a', 'abc')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('defaultBehavior')
+            ->with(null)
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('execute');
+
+        $this->assertEquals(null, $manager->invoke('Test.version.a', 'abc'));
+    }
+
+    public function testInvokeTwoParamEnabled() {
+        $config = $this->getMock('Zumba\Swivel\Config');
+        $manager = $this->getMock('Zumba\Swivel\Manager', ['forFeature'], [$config]);
+        $builder = $this
+            ->getMockBuilder('Zumba\Swivel\Builder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $manager
+            ->expects($this->once())
+            ->method('forFeature')
+            ->with('Test')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('addBehavior')
+            ->with('version.a', 'abc')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('defaultBehavior')
+            ->with('default')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue('abc'));
+
+        $this->assertEquals('abc', $manager->invoke('Test.version.a', 'abc', 'default'));
+    }
+
+    public function testInvokeTwoParamDisabled() {
+        $config = $this->getMock('Zumba\Swivel\Config');
+        $manager = $this->getMock('Zumba\Swivel\Manager', ['forFeature'], [$config]);
+        $builder = $this
+            ->getMockBuilder('Zumba\Swivel\Builder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $manager
+            ->expects($this->once())
+            ->method('forFeature')
+            ->with('Test')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('addBehavior')
+            ->with('version.a', 'abc')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('defaultBehavior')
+            ->with('default')
+            ->will($this->returnValue($builder));
+
+        $builder
+            ->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue('default'));
+
+        $this->assertEquals('default', $manager->invoke('Test.version.a', 'abc', 'default'));
+    }
 }
