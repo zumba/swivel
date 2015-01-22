62707113cac7af01896883d6bff55111e68ca89b
Stephen Young <young.steveo@gmail.com>
2015-01-22 15:39:55 -0500

    Changed `Config` to accept a map and removed feature level methods

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -4,23 +4,6 @@ namespace Zumba\Swivel;
 interface ConfigInterface extends \Psr\Log\LoggerAwareInterface {
 
     /**
-     * Add a feature to the config.
-     *
-     * @param string $slug
-     * @param array $buckets
-     * @return void
-     */
-    public function addFeature($slug, array $buckets);
-
-    /**
-     * Add an array of features to the config.
-     *
-     * @param array $map Example: [ "A" => [1,2,3], "B" => [4,5,6] ]
-     * @return void
-     */
-    public function addFeatures(array $map);
-
-    /**
      * Get a configured Bucket instance
      *
      * @return \Zumba\Swivel\Bucket
@@ -28,30 +11,11 @@ interface ConfigInterface extends \Psr\Log\LoggerAwareInterface {
     public function getBucket();
 
     /**
-     * Get the configured feature map.
-     *
-     * @return array
-     */
-    public function getFeatures();
-
-    /**
      * Get the PSR3 logger
      *
      * @return \Psr\Log\LoggerInterface
      */
     public function getLogger();
-
-    /**
-     * Remove a slug from the map.
-     *
-     * If $buckets is provided, will only remove the indicated buckets from the feature, not the
-     * entire slug.
-     *
-     * @param string $slug
-     * @param array $buckets
-     * @return void
-     */
-    public function removeFeature($slug, array $buckets = []);
 
     /**
      * Set the bucket index for the user
--- test/Tests/ConfigTest.php
+++ test/Tests/ConfigTest.php
@@ -5,55 +5,6 @@ use \Zumba\Swivel\Config,
     \Zumba\Swivel\Bucket;
 
 class ConfigTest extends \PHPUnit_Framework_TestCase {
-    public function testConstructorAddsEmptyMap() {
-        $config = new Config();
-        $this->assertEmpty($config->getFeatures());
-    }
-
-    public function testConstructorAddsMapParam() {
-        $map = ['Feature' => [1,2,3]];
-        $config = new Config($map);
-        $this->assertEquals($map, $config->getFeatures());
-    }
-
-    public function testAddFeature() {
-        $config = new Config();
-        $slug = 'Test';
-        $buckets = [1,2];
-        $config->addFeature($slug, $buckets);
-        $this->assertEquals($buckets, $config->getFeatures()[$slug]);
-    }
-
-    public function testAddFeatures() {
-        $config = new Config();
-        $features = [ 'A' => [1,2,3], 'B' => [4,5,6] ];
-        $config->addFeatures($features);
-        $this->assertEquals($features, $config->getFeatures());
-    }
-
-    public function testFeaturesAreCumulative() {
-        $config = new Config();
-        $features = [ 'A' => [1,2,3] ];
-        $config->addFeatures($features);
-        $config->addFeature('A', [9]);
-        $this->assertEquals([ 'A' => [1,2,3,9] ], $config->getFeatures());
-    }
-
-    public function testRemoveFeature() {
-        $config = new Config();
-        $features = [ 'A' => [1,2,3], 'B' => [4,5,6] ];
-        $config->addFeatures($features);
-        $config->removeFeature('B');
-        $this->assertEquals([ 'A' => [1,2,3] ], $config->getFeatures());
-    }
-
-    public function testRemoveFeatureBucketOnly() {
-        $config = new Config();
-        $features = [ 'A' => [1,2,3], 'B' => [4,5,6] ];
-        $config->addFeatures($features);
-        $config->removeFeature('B', [5]);
-        $this->assertEquals([ 'A' => [1,2,3], 'B' => [4,6] ], $config->getFeatures());
-    }
 
     public function testGetBucket() {
         $index = 4;
